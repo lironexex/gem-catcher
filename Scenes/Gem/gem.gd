e70f65dd1450d235804ac1f707b8a492999a2351
Liron Haber <liron@korentec.co.il>
2025-07-09 16:19:36 +0300

    signals for all gems

--- Scenes/Gem/gem.gd
+++ Scenes/Gem/gem.gd
@@ -3,6 +3,8 @@ extends Area2D
 const INITIAL_SPEED: float = 100.0 
 var END_OF_SCREEN_Y: float
 
+signal gem_off_screen
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	END_OF_SCREEN_Y = get_viewport_rect().end.y
@@ -18,6 +20,7 @@ func _process(delta: float) -> void:
 	
 	if position.y > END_OF_SCREEN_Y:
 		print ("gem fell out of screen")
+		gem_off_screen.emit()
 		die()
 
 func _on_area_entered(area: Area2D) -> void:
